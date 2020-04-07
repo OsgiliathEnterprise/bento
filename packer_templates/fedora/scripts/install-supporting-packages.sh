c032d4841bfca2e906fe6c96e5f6bafc3cf5d7cc
charliemordant <cmordant1@gmail.com>
2020-04-07 14:23:23 +0200

    Docker on Fedora 31
    Signed-off-by: Charlie Mordant <cmordant1@gmail.com>

--- packer_templates/fedora/scripts/install-supporting-packages.sh
+++ packer_templates/fedora/scripts/install-supporting-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eux
 # Chef with Fedora >= 30 requires libxcrypt-compat to be installed
-dnf -y install libxcrypt-compat
+dnf -y install libxcrypt-compat grubby && grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0" --make-default
